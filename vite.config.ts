d05e2d9cd9b30711e44aa5051d5eb7842a9ca0d8
Lexamplify <lexamplify@gmail.com>
2025-08-25 23:37:11 +0530

    add unplugin-auto-import to vite configuration for automatic Vue imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
-  plugins: [Vue()],
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: false,
+    }),
+  ],
   build: {
     outDir: 'dist',
     copyPublicDir: true,
